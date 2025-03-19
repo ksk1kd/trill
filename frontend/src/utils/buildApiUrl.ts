119a13e84b414d70bd4ddbfa1b41d8a336e34530
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-20 00:36:18 +0900

    fix backend url

--- frontend/src/utils/buildApiUrl.ts
+++ frontend/src/utils/buildApiUrl.ts
@@ -1,5 +1,5 @@
 import { API_PREFIX } from 'constants/api'
 
 export function buildApiUrl({ path }: { path: string }): string {
-  return 'http://trill-backend' + API_PREFIX + path
+  return 'http://trill-backend:1323' + API_PREFIX + path
 }
